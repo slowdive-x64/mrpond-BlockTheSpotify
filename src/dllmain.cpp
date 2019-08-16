ce76a7ac923ffd0e7af3ba3436d8983d8013da08
mrpond <iostream_x@hotmail.com>
2019-08-17 06:25:41 +0700

    Fix error in debug.log

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -186,30 +186,42 @@ int WSAAPI WSASend_hook(
 //#define API_COPY(M, N) \
 //	_##N = GetProcAddress(M, #N);
 
-bool AddDllToBlacklist (const wchar_t* dll_name) {
-	return true;
-}
+bool AddDllToBlacklist (const wchar_t* dll_name) { 	return true; }
 void ClearReportsBetween_ExportThunk (time_t begin, time_t end) {}
+
 int CrashForException_ExportThunk (EXCEPTION_POINTERS* info) {
-	return UnhandledExceptionFilter (info);
+	return EXCEPTION_CONTINUE_SEARCH;
 }
 void DisableHook () {}
-uint32_t DrainLog (uint8_t* buffer,	uint32_t buffer_size, uint32_t* log_remaining) {
+
+uint32_t DrainLog (uint8_t* buffer,
+	uint32_t buffer_size,
+	uint32_t* log_remaining) {
+	
 	return 0;
 }
-bool DumpHungProcessWithPtype_ExportThunk (HANDLE process_handle, const char* ptype) {
+
+bool DumpHungProcessWithPtype_ExportThunk (HANDLE process_handle,
+	const char* ptype) {
 	return false;
 }
-void DumpProcessWithoutCrash () {}
+
+void DumpProcessWithoutCrash (void* task_port) {}
+
 int32_t GetApplyHookResult () {	return 0; }
 uint32_t GetBlockedModulesCount () { return 0; }
-size_t GetCrashReports_ExportThunk (void* reports, size_t reports_size) {
+
+size_t GetCrashReports_ExportThunk (void* reports,
+	size_t reports_size) {
 	return 0;
 }
+
 const wchar_t* GetCrashpadDatabasePath_ExportThunk () {
 	return nullptr;
 }
-void GetHandleVerifier () {}
+void* GetHandleVerifier () {
+	return NULL;
+}
 uint32_t GetInstallDetailsPayload () { return 0; }
 uint32_t GetUniqueBlockedModulesCount () {	return 0; }
 bool GetUserDataDirectoryThunk (wchar_t* user_data_dir,
@@ -618,8 +630,8 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 		
 		// Only patch the main process and none of the renderers/workers
 		// this make hook not work on spotify 1.1.12.449.g4109e645
-		//	if (!wcsstr(GetCommandLine(), L"--type="))
-		CreateThread(NULL, NULL, MainThread, NULL, 0, NULL);
+		if (!wcsstr(GetCommandLine(), L"--type="))
+			CreateThread(NULL, NULL, MainThread, NULL, 0, NULL);
 		break;
 
 	}
